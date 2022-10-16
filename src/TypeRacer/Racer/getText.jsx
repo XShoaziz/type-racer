c871d8caf36026a07ba2e44c3c6110b93317921e
XShoaziz <xusanovshoaziz@gmail.com>
2022-10-16 12:04:46 +0500

    update4

--- src/TypeRacer/Racer/getText.jsx
+++ src/TypeRacer/Racer/getText.jsx
@@ -1,6 +1,6 @@
 export default () => {
   const texts = [
-    "Life is a beautiful journey that is meant to be embraced to the fullest every day. However, that doesn't mean you always wake up ready to seize the day, and sometimes need a reminder that life is a great gift.",
+    "Life is a beautiful full journey that is meant to be embraced to the fullest every day. However, that doesn't mean you always wake up ready to seize the day, and sometimes need a reminder that life is a great gift.",
     // "Many of life's failures are people who did not realize how close they were to success when they gave up.",
     // "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking.",
     // "Sing like no one's listening, love like you've never been hurt, dance like nobody's watching, and live like it's heaven on earth.",
