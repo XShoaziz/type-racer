23d1f232fcf1c30d16012dab70a3e32fff03a42e
XShoaziz <xusanovshoaziz@gmail.com>
2022-10-22 12:43:34 +0500

    yangilash

--- src/TypeRacer/Racer/Race.jsx
+++ src/TypeRacer/Racer/Race.jsx
@@ -39,7 +39,9 @@ const Race = () => {
         </p>
         <TimerDiv>
           <h3>
-            The race iss Xusanov__Sh hi hi hi,hi hi salom 1 onnn! Type the text
+            The race iss Xusanov__Sh hi hi hi,hi hi salom 
+            salom
+            1 onnn! Type the text
             below:
           </h3>
           <p>Symbols : {symbols}</p>
